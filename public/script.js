05880586c360e8088e281f16966013005d78151d
Tanishqkumarpatel <tanishq38123@gmail.com>
2026-06-11 15:44:54 -0700

    Update to support modal view, Add handlers (save, edit and cancel), fetch: Get/id and Put/id

--- public/script.js
+++ public/script.js
@@ -36,10 +36,21 @@ async function UpdateUi() {
             }
             total_balance += transaction.amount;
 
-            const button =  document.createElement("button");
-            button.innerHTML = "Delete";
-            button.onclick = () => handleDelete(transaction.id);
-            t.appendChild(button);
+            const del_button =  document.createElement("button");
+            del_button.innerHTML = "Delete";
+            del_button.id = "delete-btn";
+            del_button.onclick = () => handleDelete(transaction.id);
+            
+            const edit_button = document.createElement("button");
+            edit_button.innerHTML = "Edit";
+            edit_button.id = "edit-btn";
+            edit_button.onclick = () => handleEdit(transaction.id);
+
+            const btn_group = document.createElement("div");
+            btn_group.appendChild(edit_button);
+            btn_group.appendChild(del_button);
+            
+            t.appendChild(btn_group);
             list.append(t);
         });
         Balance.innerText = total_balance;
@@ -89,4 +100,60 @@ async function handleDelete(idx) {
         UpdateUi();
     }
    
+}
+
+async function handleEdit(id) {
+    
+    try {
+        const res = await fetch(`/transactions/${id}`, {
+            method: "GET",
+        });
+        if (!res.ok) {
+            throw new Error(`Get Error: ${res.status}`)
+        }
+        const data = await res.json()
+        let description = document.getElementById("modal-name")
+        let amount = document.getElementById("modal-amount")
+        description.value = data.description
+        amount.value = data.amount
+        document.getElementById("modal-overlay").style.display = "flex"
+        document.getElementById("modal-box").dataset.id = id;
+    } catch (error) {
+        console.log(error)
+        console.error(error);
+    }
+}
+
+async function handleSave() {
+
+    const description =  document.getElementById("modal-name").value
+    const amount = parseFloat(document.getElementById("modal-amount").value)
+    const id =  document.getElementById("modal-box").dataset.id
+    if (!description && isNaN(amount)) {
+        alert("Please enter both a name and a valid amount.");
+    }
+    try {
+        const res = await fetch(`/transactions/${id}`, {
+            method: "PUT",
+            body: JSON.stringify({
+                "description": description,
+                "amount": amount
+            }),
+            headers: {"Content-Type": "application/json"}
+        })
+        if (!res.ok) {
+            throw new Error(`PUT Error: ${res.status}`)
+        }
+        alert("Edit successful")
+    } catch (error) {
+        alert(error)
+        console.error(error)
+    } finally {
+        document.getElementById("modal-overlay").style.display = "none"
+        UpdateUi()
+    }
+}
+
+function handleCancel() {
+    document.getElementById("modal-overlay").style.display = "none"
 }
